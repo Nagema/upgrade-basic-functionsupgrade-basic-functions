1bf5d487871fddc5cf324602a123f05f3fc534f2
angelica <angelica.cavero@gmail.com>
2022-10-06 09:46:27 +0200

    functions

---
+++ iteracion1/index.js
@@ -0,0 +1,11 @@
+
+const num1 = 5;
+const num2 = 3
+
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log(`${numberOne} is the higher number`)
+    }
+  };
+  
+sum(num1, num2);
