27b5e6e56c12e0f8295ff5e52b85f2acfa63d36d
angelica <angelica.cavero@gmail.com>
2022-10-06 10:10:53 +0200

    function4

---
+++ iteracion4/index.js
@@ -0,0 +1,13 @@
+// Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
+
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+let num = 0;
+function average(numbersList) {
+ for (let index = 0; index < numbersList.length; index++) {
+     const element = numbersList[index];
+     num = element + num
+    }
+    return num / numbersList.length
+}
+
+console.log(average(numbers))
